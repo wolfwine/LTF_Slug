63efc7dead2bbc5f77c8dbdafa488759f883a7d3
egaouyer@gmail.com <egaouyer@gmail.com>
2020-04-15 17:11:02 +0200

    removed 1.1 directory; Added giverDefSet; Using MoarHediffs dll

--- Source/LTF_Slug/Tools.cs
+++ Source/LTF_Slug/Tools.cs
@@ -10,85 +10,19 @@ using UnityEngine;
 using Verse;
 
 
-namespace LighterThanFast
+namespace LTF_Slug
 {
     public class Tools
     {
-
-        public static string OkStr(bool boolean=false)
+        public static bool CheckPawn(Pawn pawn)
         {
-            return "[" + ((boolean) ? ("OK") : ("KO")) + "]";
+            //return (pawn != null && pawn.Map != null && pawn.Position != null);
+            return (pawn != null && pawn.Map != null);
         }
-
         public static void Warn(string warning, bool debug = false)
         {
             if(debug)
                 Log.Warning(warning);
-        }
-        public static void WarnRare(string warning, int period=300, bool debug = false)
-        {
-            if (debug)
-            {
-                bool display = ((Find.TickManager.TicksGame % period)==0);
-                if (display)
-                    Log.Warning(warning);
-            }
-        }
-
-        //Quality
-        public static bool BestQuality(CompQuality compQuality)
-        {
-            if (compQuality == null)
-                return false;
-            return (compQuality.Quality == QualityCategory.Legendary);
-        }
-        public static bool WorstQuality(CompQuality compQuality)
-        {
-            if (compQuality == null)
-                return false;
-            return (compQuality.Quality == QualityCategory.Awful);
-        }
-        public static string BetterQuality(CompQuality comp)
-        {
-            return (VirtualQuality(comp, 1));
-        }
-        public static string WorseQuality(CompQuality comp)
-        {
-            return (VirtualQuality(comp, -1));
-        }
-        
-        public static string VirtualQuality(CompQuality comp, int relativeChange = 0)
-        {
-            string answer = "no quality comp";
-            if (comp != null)
-                //answer = comp.Quality.AddLevels(relativeChange).GetLabelShort();
-                // ERROR THIS IS FALSE
-                answer = comp.Quality.GetLabelShort();
-
-            return (answer);
-        }
-        
-        public static string PawnResumeString(Pawn pawn)
-        {
-            return (pawn?.LabelShort.CapitalizeFirst() +
-                    ", " +
-                    (int)pawn?.ageTracker?.AgeBiologicalYears + " y/o" +
-                    //" " + pawn?.gender.ToString()?.Translate()?.ToLower() +
-                    " " + pawn?.gender.GetLabel() + 
-                    ", " + pawn?.def?.label + "("+pawn.kindDef+")"
-                    );
-        }
-
-        //debug Toggle kinda pointless
-        public static string DebugStatus(bool debug)
-        {
-            return (debug+ "->" + !debug);
-        }
-
-        //PauseOnError for debug purpose
-        public static void PauseOnErrorToggle()
-        {
-            Prefs.PauseOnError = !Prefs.PauseOnError;
         }
 
     }
---
+++ Source/LTF_Slug/ToolsPawn.cs
@@ -0,0 +1,36 @@
+﻿using RimWorld;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using Verse;
+
+
+namespace LTF_Slug
+{
+    public class ToolsPawn
+    {
+        public static BodyPartRecord GetBrain(Pawn pawn)
+        {
+            pawn.RaceProps.body.GetPartsWithTag(BodyPartTagDefOf.ConsciousnessSource).TryRandomElement(out BodyPartRecord bodyPart);
+            return bodyPart;
+        }
+
+        public static BodyPartRecord GetStomach(Pawn pawn)
+        {
+            pawn.RaceProps.body.GetPartsWithDef(BodyPartDefOf.Stomach).TryRandomElement(out BodyPartRecord bodyPart);
+            return bodyPart;
+        }
+
+        public static void Warn(string warning, bool debug = false)
+        {
+            if(debug)
+                Log.Warning(warning);
+        }
+
+    }
+}
