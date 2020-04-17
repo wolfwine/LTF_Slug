ef16a8a1f608fdcf9b23edcd2a0f0f11255929fe
egaouyer@gmail.com <egaouyer@gmail.com>
2020-04-17 23:13:32 +0200

    Added the ability to create a mind flay spot

---
+++ Source/LTF_Slug/LTF_Slug_MindFlayAbilityUser.cs
@@ -0,0 +1,172 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using AbilityUser;
+using AlienRace;
+using UnityEngine;
+
+using System;
+using System.Diagnostics;
+using System.Text;
+
+using Verse.Sound;
+
+namespace LTF_Slug
+{
+    [StaticConstructorOnStartup]
+    public static class TexButton
+    {
+        public static readonly Texture2D MindFlayerAbility = ContentFinder<Texture2D>.Get("UI/MindFlay", true);
+    }
+
+    // reference: https://github.com/roxxploxx/RimWorldModGuide/wiki/SHORTUTORIAL%3A-JecsTools.CompAbilityUser
+
+    public class CompMindFlayer : GenericCompAbilityUser 
+    {
+        public bool myDebug = true;
+
+        public bool? MindFlayer;
+        
+        // Provides ability without affecting save.a
+        public override void CompTick()
+        {
+            //Log.Warning("CompTick");
+            if (AbilityUser?.Spawned == true)
+            {
+                if (MindFlayer != null)
+                {
+                    if (MindFlayer == true)
+                    {
+                        base.CompTick();
+                    }
+                }
+                else
+                {
+                    MindFlayer = TryTransformPawn();
+                    Initialize();
+                }
+            }
+
+        }
+
+        public override void PostInitialize()
+        {
+            base.PostInitialize();
+            if (MindFlayer == true) this.AddPawnAbility(MindFlayerDefOf.LTF_Slug_MindFlayer);
+        }
+
+        private BodyPartRecord GetHeart(Pawn pawn)
+        {
+            BodyPartRecord bodyPart = null;
+            //pawn.RaceProps.body.GetPartsWithTag("BloodPumpingSource").TryRandomElement(out bodyPart);
+            pawn.RaceProps.body.GetPartsWithTag(BodyPartTagDefOf.BloodPumpingSource).TryRandomElement(out bodyPart);
+            if (bodyPart == null)
+            {
+                Tools.Warn("null heart ?", myDebug);
+            }
+
+            return bodyPart;
+        }
+
+
+        public bool IsMindFlayer
+        {
+            get
+            {
+                bool val = false;
+                if (this.AbilityUser == null)
+                    return false;
+
+                // race
+                val = IsSlug(AbilityUser);
+                if (!val)
+                {
+                    Tools.Warn(AbilityUser.LabelShort + " is not Crystal", myDebug);
+                    return false;
+                }
+
+                // heart
+                BodyPartRecord heart = GetHeart(AbilityUser);
+                if (heart == null)
+                {
+                    Tools.Warn(AbilityUser.LabelShort + " has no heart", myDebug);
+                    return false;
+                }
+
+                // crysta heart
+                if (heart.def.defName != "Heart")
+                {
+                    Tools.Warn(AbilityUser.LabelShort + " has a non legit " + heart.def.defName, myDebug);
+                    return false;
+                }
+
+
+                // crystal clear health
+                List<Hediff> myHediffs = AbilityUser.health.hediffSet.hediffs;
+                for (int i = 0; i < myHediffs.Count; i++)
+                {
+                    BodyPartRecord myBP = myHediffs[i].Part;
+
+                    if (myBP != heart) {
+                        continue;
+                    }
+                    else
+                    {
+                        Tools.Warn(AbilityUser.LabelShort + " is special", myDebug);
+                        val = false;
+                        break;
+                    }
+                }
+
+                return val;
+            }
+        }
+
+
+        private bool IsSlug(Pawn pawn)
+        {
+            return (pawn?.def.defName == "Alien_Slug");
+        }
+
+        public override bool TryTransformPawn()
+        {
+            //Log.Warning("TryTransformPawn");
+            //if (!LockdownerMod.settings.enableLockdownerAbility) return false;
+            return IsMindFlayer;
+        }
+
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            if (AbilityUser.Drafted)
+            {
+                IEnumerator<Gizmo> enumerator = base.CompGetGizmosExtra().GetEnumerator();
+                while (enumerator.MoveNext())
+                {
+                    Gizmo current = enumerator.Current;
+                    yield return current;
+                }
+                for (int i = 0; i < this.AbilityData.AllPowers.Count; i++)
+                    yield return this.AbilityData.AllPowers[i].GetGizmo();
+            }
+        }
+
+    }
+
+    [DefOf]
+    public static class MindFlayerDefOf
+    {
+        public static AbilityUser.AbilityDef LTF_Slug_MindFlayer;
+    }
+
+    public class MindFlayerSpark : Projectile_AbilityBase
+    {
+        protected override void Impact(Thing hitThing)
+        {
+            base.Impact(hitThing);
+            //LockdownerUtility.CreateLockBlocks(hitThing, DestinationCell);
+            MindFlayerUtility.CreateMindFlaySpot(hitThing);
+        }
+    }
+
+}
---
+++ Source/LTF_Slug/LTF_Slug_MindFlayEffect.cs
@@ -0,0 +1,124 @@
+﻿using RimWorld;
+using AlienRace;
+using Verse;
+using Verse.AI;
+using UnityEngine;
+using System.Collections.Generic;
+using AbilityUser;
+
+using System;
+
+
+namespace LTF_Slug
+{
+
+    public class MindFlayEffect : AbilityUser.Verb_UseAbility
+    {
+        private const bool myDebug = true;
+
+        public override bool CanHitTargetFrom(IntVec3 root, LocalTargetInfo targ)
+        {
+            Tools.Warn("CanHitTargetFrom : Blink in TryCastShot with root" + root + " and target" + targ, myDebug);
+
+            if (UseAbilityProps.AbilityTargetCategory == AbilityTargetCategory.TargetLocation)
+            {
+                Tools.Warn("AbilityTargetCategory == TargetLocation; base.CanHitTargetFrom", myDebug);
+                return base.CanHitTargetFrom(root, targ);
+            }
+            Tools.Warn("??? AbilityTargetCategory != TargetLocation; TryFindShootLineFromTo", myDebug);
+
+            return TryFindShootLineFromTo(root, targ, out ShootLine shootLine);
+        }
+
+        protected override bool TryCastShot()
+        {
+            Tools.Warn("Entering TryCastShot", myDebug);
+
+            if (currentTarget != null && CasterPawn != null && currentTarget.Cell != null && currentTarget.Cell.IsValid)
+            {
+                Tools.Warn("Valid target in TryCastShot", myDebug);
+                CreateMindFlaySpot(currentTarget.Cell, CasterPawn.Map);
+                Tools.Warn("created MindFlaySpot in TryCastShot", myDebug);
+                return true;
+                //this.CasterPawn.SetPositionDirect(this.currentTarget.Cell);
+            }
+            else
+            {
+                Tools.Warn("failed to TryCastShot", myDebug);
+            }
+
+            // Setting cooldown
+            //ability.TicksUntilCasting = (int)this.UseAbilityProps.SecondsToRecharge * GenTicks.TicksPerRealSecond;
+
+            return false;
+        }
+        /*
+        public virtual void Effect()
+        {
+            string message = string.Empty;
+
+            CreateLockBlocks(CasterPawn);
+            MoteMaker.ThrowText(CasterPawn.DrawPos, CasterPawn.Map, message);
+        }
+        */
+
+        //IntVec3
+        public static IntVec3 InvertIntVec3(IntVec3 intVec3)
+        {
+            IntVec3 temp;
+            int x, y, z;
+            x = intVec3.x; y = intVec3.y; z = intVec3.z;
+
+            x *= -1;
+            z *= -1;
+
+            temp = new IntVec3(x, y, z);
+            return (temp);
+        }
+        
+        public static void CreateMindFlaySpot(IntVec3 destinationCell, Map map)
+        {
+            Building mindFlaySpot = (Building)ThingMaker.MakeThing(ThingDef.Named("LTF_MindFlaySpot"), null);
+
+            //lockBlock.SetColor(lockDowner.DrawColor);
+            GenSpawn.Spawn(mindFlaySpot, destinationCell, map, Rot4.North, WipeMode.Vanish);
+
+            ThrowMicroFlakes(destinationCell.ToVector3(), map);
+
+            foreach (IntVec3 puff in GenAdj.CellsAdjacent8Way(destinationCell, Rot4.North, IntVec2.North))
+                if(puff.InBounds(map))
+                    MoteMaker.ThrowAirPuffUp(puff.ToVector3(), map);
+        }
+
+        public static void ThrowMicroFlakes(Vector3 loc, Map map)
+        {
+            if (!loc.ShouldSpawnMotesAt(map) || map.moteCounter.SaturatedLowPriority)
+            {
+                return;
+            }
+            MoteThrown moteThrown = (MoteThrown)ThingMaker.MakeThing(ThingDef.Named("Mote_MindFlay"), null);
+            moteThrown.Scale = Rand.Range(0.5f, 1.2f);
+            moteThrown.rotationRate = Rand.Range(-12f, 36f);
+
+            moteThrown.exactPosition = loc;
+            moteThrown.exactPosition -= new Vector3(0.5f, 0f, 0.5f);
+            moteThrown.exactPosition += new Vector3(Rand.Value, 0f, Rand.Value);
+            moteThrown.SetVelocity((float)Rand.Range(35, 45), 1.2f);
+
+            GenSpawn.Spawn(moteThrown, loc.ToIntVec3(), map);
+            GenSpawn.Spawn(moteThrown, loc.ToIntVec3(), map);
+            GenSpawn.Spawn(moteThrown, loc.ToIntVec3(), map);
+        }
+
+        // Necessary for autocomplete ability
+        public override void PostCastShot(bool inResult, out bool outResult)
+        {
+            if (inResult)
+            {
+                //Effect();
+                outResult = true;
+            }
+            outResult = inResult;
+        }
+    }
+}
